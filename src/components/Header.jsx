62a94492f0cb0c416a88abeeb87ea92258c8ceec
Macey Bell <maceytbell@gmail.com>
2023-02-21 15:42:25 -0700

    math tutoring app header progress

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ import logo from './images/logo.png'
+ 
+ const Header = () => {
+   return (
+     <header>
+        <img className='logo' src={logo} alt="" />
+     </header>
+   )
+ }
+ 
+ export default Header
