101ce73a4194cbba4916084845f289c672b31d55
Macey Bell <maceytbell@gmail.com>
2023-02-21 20:30:43 -0700

    math hw app header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
  import React from 'react'
  import logo from './images/logo.png'
+ import {BiMenu} from 'react-icons/bi'
  
  const Header = () => {
    return (
      <header>
         <img className='logo' src={logo} alt="" />
+        <BiMenu className='menu' size="40px" />    
      </header>
    )
  }
